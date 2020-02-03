8560aaa71b087f6448735942fa7d2a8a8d35c37d
David Raynes <rayners@gmail.com>
2020-02-03 18:19:04 -0500

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
-  env: {
-    node: true
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      "arrowFunctions": true,
-      "blockBindings": true,
-      "classes": true,
-      "defaultParameters": true,
-      "destructuring": true,
-      "forOf": true,
-      "generators": true,
-      "modules": true,
-      "objectLiteralComputedProperties": true,
-      "objectLiteralDuplicateProperties": true,
-      "objectLiteralShorthandMethods": true,
-      "objectLiteralShorthandProperties": true,
-      "regexUFlag": true,
-      "regexYFlag": true,
-      "restParams": true,
-      "spread": true,
-      "superInFunctions": true,
-      "templateStrings": true,
-      "unicodeCodePointEscapes": true,
-      "globalReturn": true
+    env: {
+        node: true
     },
-      ecmaVersion: 6,
-  },
-  extends: ['eslint:recommended'],
-  overrides: [
-    {
-      files: ["*.test.js"],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    parserOptions: {
+        ecmaFeatures: {
+            arrowFunctions: true,
+            blockBindings: true,
+            classes: true,
+            defaultParameters: true,
+            destructuring: true,
+            forOf: true,
+            generators: true,
+            modules: true,
+            objectLiteralComputedProperties: true,
+            objectLiteralDuplicateProperties: true,
+            objectLiteralShorthandMethods: true,
+            objectLiteralShorthandProperties: true,
+            regexUFlag: true,
+            regexYFlag: true,
+            restParams: true,
+            spread: true,
+            superInFunctions: true,
+            templateStrings: true,
+            unicodeCodePointEscapes: true,
+            globalReturn: true
+        },
+        ecmaVersion: 6
+    },
+    extends: ["eslint:recommended"],
+    overrides: [
+        {
+            files: ["*.test.js"],
+            env: {
+                jest: true
+            }
+        }
+    ]
 };
