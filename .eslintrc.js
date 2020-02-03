c631e7bfb77cc9a41ac3dc6db868f6aebe6a89c6
David Raynes <rayners@gmail.com>
2020-02-03 18:01:22 -0500

    Pre-class package/code updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,30 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['eslint:recommended']
+  parserOptions: {
+    ecmaFeatures: {
+      "arrowFunctions": true,
+      "blockBindings": true,
+      "classes": true,
+      "defaultParameters": true,
+      "destructuring": true,
+      "forOf": true,
+      "generators": true,
+      "modules": true,
+      "objectLiteralComputedProperties": true,
+      "objectLiteralDuplicateProperties": true,
+      "objectLiteralShorthandMethods": true,
+      "objectLiteralShorthandProperties": true,
+      "regexUFlag": true,
+      "regexYFlag": true,
+      "restParams": true,
+      "spread": true,
+      "superInFunctions": true,
+      "templateStrings": true,
+      "unicodeCodePointEscapes": true,
+      "globalReturn": true
+    },
+      ecmaVersion: 6,
+  },
+  extends: ['eslint:recommended'],
 };
