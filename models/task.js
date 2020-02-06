c16a4458cb7e799f21bbc3ac643cb1533d505366
David Raynes <rayners@gmail.com>
2020-02-05 20:59:41 -0500

    In class 4 work

---
+++ models/task.js
@@ -0,0 +1,15 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+    const Task = sequelize.define(
+        "Task",
+        {
+            name: DataTypes.STRING,
+            description: DataTypes.STRING
+        },
+        {}
+    );
+    // Task.associate = function(models) {
+    //   // associations can be defined here
+    // };
+    return Task;
+};
