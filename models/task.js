e6e1fc327f23ae8f79f9c75149b20c274b6e0299
David Raynes <rayners@gmail.com>
2020-02-10 21:14:58 -0500

    Scopes!

--- models/task.js
+++ models/task.js
@@ -1,4 +1,5 @@
 "use strict";
+const Sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
     const Task = sequelize.define(
         "Task",
@@ -7,17 +8,49 @@ module.exports = (sequelize, DataTypes) => {
             description: DataTypes.STRING,
             completedAt: DataTypes.DATE
         },
-        {}
+        {
+            scopes: {
+                emptyDesc: {
+                    where: {
+                        description: { [Sequelize.Op.ne]: "" }
+                    }
+                },
+                incomplete: {
+                    where: {
+                        completedAt: null
+                    }
+                },
+                completedWithinAnHour: function() {
+                    return {
+                        where: {
+                            completedAt: {
+                                [Sequelize.Op.gt]: new Date() - 60 * 60 * 1000
+                            }
+                        }
+                    };
+                },
+                completedWithinXHours: function(x) {
+                    return {
+                        where: {
+                            completedAt: {
+                                [Sequelize.Op.gt]:
+                                    new Date() - 60 * 60 * 1000 * x
+                            }
+                        }
+                    };
+                }
+            }
+        }
     );
     // Task.associate = function(models) {
     //   // associations can be defined here
     // };
 
-    Task.prototype.isCompleted = function () {
+    Task.prototype.isCompleted = function() {
         return !!this.completedAt;
     };
 
-    Task.prototype.markCompleted = async function () {
+    Task.prototype.markCompleted = async function() {
         const timeNow = new Date();
 
         return this.update({
