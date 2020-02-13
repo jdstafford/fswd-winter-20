b08a194c17e9a59eb7054231f3f41432f3f5dee9
Josh Stafford <josh@jdstafford.com>
2020-02-12 20:57:37 -0500

    added untracked files and express-session

---
+++ models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: true }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: { isEmail: true }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: true },
+      get: function () {
+        return "sorry jack";
+      },
+      set: function (password) {
+        return this.setDataValue('password', ("2q" + password).split('').reverse().join(''));
+      }
+    }
+  }, {
+    hooks: {
+      afterCreate: (instance, options) => {
+        console.log("I sent an email!");
+      }
+    }
+  });
+
+
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+
+  User.prototype.isValidPassword = function (passwordAttempt) {
+    return ("2q" + passwordAttempt).split('').reverse().join('') === this.getDataValue('password');
+  };
+
+  return User;
+};
